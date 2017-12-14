b626b82018175f35b876a334f7cc01f40f16b7c7
Raphael Osorio <raphaelaosorio@gmail.com>
2017-12-14 07:18:00 -0800

    implemented skeleton of latest state machine that i sketched out on my yellow notebook. attempting to follow state function approach by rob pike now.

--- parse.js
+++ parse.js
@@ -1,25 +1,52 @@
 function Parser() {
-  let position = 0;
+  let pos = 0;
 
   function ignore() {
-    position += 1;
+    pos += 1;
   }
 
   function next() {
-    position += 1;
+    pos += 1;
   }
 
   return {
     ignore,
-    position,
+    next,
+    get position () { return pos },
+    set position (x) { pos = x },
   };
 }
+
+exports.Parser = Parser;
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+const ENV = {
+  '+': add,
+  '-': subtract,
+  '*': multiply,
+  '/': divide,
+};
 
 function tokenize(sequence) {
   return sequence.split('');
 }
 
-function isDigit(char) {
+function isValidDigit(char) {
   switch (char) {
     case '1':
       return true;
@@ -46,7 +73,7 @@ function isDigit(char) {
   }
 }
 
-function isOperator(char) {
+function isValidOperator(char) {
   switch (char) {
     case '+':
       return true;
@@ -61,12 +88,110 @@ function isOperator(char) {
   }
 }
 
-function expression(tokens, parser) {
+function isDecimal(char) {
+  return char === '.';
+}
+
+function isValidEndToNumber (char) {
+  switch (char) {
+    case '(':
+      return true;
+    case ')':
+      return true;
+    case ' ':
+      return true;
+    default:
+      return isValidOperator(char);
+  }
+}
+/*
+still need to check for end of tokens
+*/
+function readNumber(tokens, parser) {
+  let numericalStr = '';
+  let isDecimal = false;
+  let isComplete = false;
+  let isNegative
+  let currentToken;
+  while(!isComplete) {
+    currentToken = tokens[parser.position];
+    if (!isDecimal && currentToken === '.') {
+      numericalStr += currentToken;
+      isDecimal = true;
+      parser.next();
+    }
+    else if isValidDigit(currentToken) {
+      numericalStr += currentToken;
+      parser.next();
+    }tokens[parser.position]
+    else if (isValidEndToNumber(currentToken)) {
+      isComplete = true;
+    }
+    else {
+      return TypeError('Incorrect numerical input at position: ' +  parser.position);
+    }
+  }
+  return parseFloat(numericalStr);
+}
+
+exports.readNumber = readNumber;
+
+function nextChar(tokens, parser) {
   while (tokens[parser.position] === ' ') {
     parser.ignore();
   }
-  if (isOperator(tokens[paser.position])) {
+  return tokens[parser.position];
+}
 
+function readExpression (tokens, parser) {
+  let currentToken = tokens[parser.position];
+  let expression = [];
+  expression.push(currentToken); // push operator
+  let continues = true;
+  while (continues) {
+    currentToken = nextChar(tokens, parser);
+    switch (currentToken) {
+      case '(':
+        expression.push(read(tokens, parser));
+        parser.next();
+      case ')':
+        return expression;
+      default:
+        if (isValidDigit(currentToken) || isDecimal(currentToken)) {
+          expression.push(readNumber(tokens, parser));
+          parser.next();
+        }
+    }
+  }
+    if (isValidDigit(currentToken)) {
+      const firstArg = readNumber(tokens, parser);
+      currentToken = nextChar(tokens, parser);
+      if (currentToken === ')') {
+        if (operator === '+') {
+          expression.push(float);
+        } else if (operator === '-') {
+          expression.push(float * (-1));
+        } else {
+          console.log('syntax error at position:', parser.position);
+        }
+      } else if (isValidDigit(currentToken)) {
+        const secondArg = readNumber(tokens, parser);
+        expression.push(se)
+      }
+    }
+  }
+}
+
+function read(tokens, parser) {
+  let currentToken = nextChar(tokens, parser);
+  if (currentToken === '(') {
+    if (isValidOperator(currentToken)) {
+      readExpression(tokens, parser);
+    } else {
+      console.log('invalid syntax at position: '+ parser.position + '. Expecting operator to denote an operation to be performed or a real number');
+    }
+  } else {
+      console.log('invalid syntax at position: '+ parser.position + '. Expecting opening brace to denote an expression');
   }
 }
 
@@ -74,7 +199,7 @@ function buildAST(tokens, parser) {
   if (!tokens || tokens.length === 0) {
     return;
   }
-  return expression(tokens, parser);
+  return read(tokens, parser);
 }
 
 function parse(programSequence) {
@@ -91,13 +216,15 @@ function parse(programSequence) {
 
 exports.parse = parse;
 
+
+
 /*
   Sequence has already been screened for syntax errors, all strings expected to be operators
 */
 function evaluate(x) {
   if (typeof x === 'number') {
     return x;
-  } else { // expression
+  } else { // read expression
     const procedure = x[0]; // arithmetic procedure
     const args = x.slice(1, x.length).map((y) => evaluate(y));
     return procedure(args[0], args[1]);
@@ -107,52 +234,20 @@ function evaluate(x) {
 exports.eval = evaluate;
 
 
-
-
-function parseNumbers(sequence) {
-
-  console.log('number:', parseFloat(sequence));
-}
-exports.parseNumbers = parseNumbers;
-function validateArithmetic(sequence) {
-  if (sequence.match(/\d+(\.\d+)?((\+|\-|\*|\/)(\d+(\.\d+)?))?$/g)) {
-    console.log('valid arithmetic');
-  } else {
-    console.log('invalid arithmetic');
-  }
-}
-exports.validateArithmetic = validateArithmetic;
-
-
-
-
-
-
-
-
-
-// function readFromTokens(tokens, ast) {
-//   if (tokens.length === 0 && !state) {
-//     return ast;
+// function peek (tokens, parser) {
+//   nextChar(tokens, parser);
+//   const startPosition = parser.position;
+  
+//   let currentToken;
+//   currentToken = tokens[parser.position];
+//   if (currentToken === '-' || currentToken === '+') {
+//     let isAtom = false;
+//     let isExpression = false;
+//     while (!isAtom || ~isExpression) {
+//       nextChar(tokens, parser);
+//       currentToken = tokens[parser.position];
+//       if (currentToken)
+//     } 
 //   }
-//   else if (tokens[0] === ' ') {
-//     readFromTokens(tokens.splice(1, tokens.length), ast, state);
-//   }
-//   else if (tokens[0] === '(') {
-//     const newAst = readExpression(tokens.splice(1, tokens.length));
-//   }
-//   else if (tokens[0].match(/(\+|\-|\*|\/)/) && state.match('expectingOperator')) {
-//     ast.push(tokens[0]);
-//     return readFromTokens(tokens.splice(1, tokens.length), ast);
-//   }
-//   else if (tokens[0].match(/\d/)) {
-//     const { number, remainingTokens } = readNumber(tokens);
-//     ast.push(parseFloat(number));
-//     return readFromTokens(remainingTokens, ast);
-//   }
-//   else if (tokens[0] === ')' && state === false) {
-//     return ast;
-//   } else {
-//     // throw error
-//   }
+//   nextChar();
 // }
